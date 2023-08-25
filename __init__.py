a61476d035a62e9b040d3c475fe32367c06aeda3
Anil Panta <panta@padminpanta-lpt.jlab.org>
2023-08-25 10:06:58 -0400

    added all required things

---
+++ __init__.py
@@ -0,0 +1,11 @@
+
+from .path_gen import construct_surl_jlab
+from .lfn2pfn import lfn2pfn_jlab
+#from .extract_scope import extract_scope_jlab
+
+SUPPORTED_VERSION = [ "1.30", "1.31", "32" ]
+
+def get_algorithms():
+    return {'lfn2pfn': {'belleii': lfn2pfn_jlab},
+            'surl': {'belleii': construct_surl_jlab}
+           }
