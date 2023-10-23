fc45ba10fd0cd5b9a19ac26f8c42f3052f95b9d0
Anil Panta <panta@padminpanta-lpt.jlab.org>
2023-10-23 13:54:05 -0400

    sort of test final version

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ from .extract_scope import extract_scope_jlab
 SUPPORTED_VERSION = [ "1.30", "1.31", "32" ]
 
 def get_algorithms():
-    return {'lfn2pfn': {'belleii': lfn2pfn_jlab},
-            'surl': {'belleii': construct_surl_jlab},
-            'scope': {'belleii': extract_scope_jlab}
+    return {'lfn2pfn': {'jlab': lfn2pfn_jlab},
+            'surl': {'jlab': construct_surl_jlab},
+            'scope': {'jlab': extract_scope_jlab}
            }
 
