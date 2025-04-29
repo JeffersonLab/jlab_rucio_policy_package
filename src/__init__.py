5cbf2f56e28212fc846157cd067c491967c3838b
Anil Panta <47672624+panta-123@users.noreply.github.com>
2025-04-29 02:28:48 +0000

    new structure

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,8 @@
-
-from .path_gen import construct_surl_jlab
-from .lfn2pfn import lfn2pfn_jlab
-from .extract_scope import extract_scope_jlab
-
-SUPPORTED_VERSION = [ "1.30", "1.31", "32" ]
+SUPPORTED_VERSION = '~=36.0'
 
 def get_algorithms():
+    from jlab_rucio_policy_package.lfn2pfn import lfn2pfn_jlab
+    from jlab_rucio_policy_package.extract_scope import extract_scope_jlab
     return {'lfn2pfn': {'jlab': lfn2pfn_jlab},
             'surl': {'jlab': construct_surl_jlab},
             'scope': {'jlab': extract_scope_jlab}
